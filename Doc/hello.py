d7f22b5681c99c17d9f5fe7e73407589bfb7c8d0
Louie Liu <louieusc@gmail.com>
2022-09-13 15:18:44 -0500

    make a py file

---
+++ Doc/hello.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#from tkinter import N
+
+
+print ('hello, world!')
+a = 123 # a是整数
+print(a)
+a = 'ABC' # a变为字符串
+print(a)
+
+a = 'ABC'
+b = a
+a = 'XYZ'
+print(b)
+
+10 % 3
+10 // 3
+10 / 3
+
+n = 123
+print (n)
+f = 456.789
+print (f)
+s1 = "Hello, \'Adam\'"
+print (s1)
+s3 = 'Hello, "Bart"'
+print (s3)
+s4 = 'Hello, \nLisa!'
+print (s4)
+
+ord('中')
+'中文'.encode('utf-8')
+b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf8')
+len('abcdefg')
+len('中文算')
