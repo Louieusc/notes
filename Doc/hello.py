8d53d7679f5f858dcb08ae032d2546cc404f9be5
Louie Liu <louieusc@gmail.com>
2022-09-18 23:04:02 -0500

    updated codes

--- Doc/hello.py
+++ Doc/hello.py
@@ -182,3 +182,15 @@ x = ("apple", "banana", "cherry")
 y = ("orange",)
 thetuple = x + y
 print(thetuple)
+
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x) 
+
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = fruits
+print(green)
+print(yellow)
+print(red)
